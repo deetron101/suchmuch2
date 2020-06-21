3cc3fc2ddd9baafa06dc1a7f03222b0871c1b7f5
Deirdre O'Regan <deirdre.oregan@gmail.com>
2020-06-21 22:09:21 +0100

    Small bit more styling

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -5,20 +5,19 @@
 	<title>Skeen</title>
 </svelte:head>
 
-<!-- Wide center column -->
-<div>
+<div class="w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
+  <h1 class="h1">Skeen suggests skincare products and routines based on your personal skin profile</h1>
+  <button class="btn btn-peach">Start with the skin quiz</button>
+  <a class="btn-link" href="features">Learn more</a>
+</div>
 
-  <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/3">
-  	<h1 class="h1">Skeen suggests skincare products and routines based on your personal skin profile</h1>
-	<button class="btn btn-peach">Start with the skin quiz</button>
-	<a href="features" class="btn-link">Learn more</a>
+<div class="max-w-lg rounded overflow-hidden shadow-lg my-16">
+  <div class="px-6 py-4">
+    <h2 class="text-brown font-bold py-4">Skin</h2>
+    <p class="text-brown text-sm">
+      Tell us about your skin and what it needs
+    </p>
   </div>
-
-  <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-    <div class="mb-8">
-      <div class="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>
-      <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
-    </div>
+  <div class="px-6 py-4">
   </div>
-
 </div>

08d4e22f38897bdfb9677e3955c4500ecd7ba06a
Deirdre O'Regan <deirdre.oregan@gmail.com>
2020-06-28 22:44:42 +0100

    Three cards

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -5,19 +5,31 @@
 	<title>Skeen</title>
 </svelte:head>
 
-<div class="w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
+<div class="title">
   <h1 class="h1">Skeen suggests skincare products and routines based on your personal skin profile</h1>
   <button class="btn btn-peach">Start with the skin quiz</button>
   <a class="btn-link" href="features">Learn more</a>
 </div>
 
-<div class="max-w-lg rounded overflow-hidden shadow-lg my-16">
-  <div class="px-6 py-4">
-    <h2 class="text-brown font-bold py-4">Skin</h2>
-    <p class="text-brown text-sm">
-      Tell us about your skin and what it needs
-    </p>
-  </div>
-  <div class="px-6 py-4">
-  </div>
+<div class="card">
+  <h2 class="h2">Skin</h2>
+  <p class="sub-head-1">
+    Tell us about your skin and what it needs
+  </p>
+  <button class="btn btn-peach">Start with the skin quiz</button>
+</div>
+
+<div class="grid grid-cols-2 gap-8">
+<div class="card">
+  <h2 class="h2">Products</h2>
+  <p class="sub-head-2">
+    Choose products you already use to find new effective products
+  </p>
+</div>
+<div class="card">
+  <h2 class="h2">Routines</h2>
+  <p class="sub-head-2">
+    Find routines that work for you and your skin
+  </p>
+</div>
 </div>

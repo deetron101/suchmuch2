33b70f6c1be27c649761341534001b92f2466dba
Deirdre O'Regan <deirdre.oregan@gmail.com>
2020-07-03 14:03:17 +0100

    Button color change

--- frontend/src/components/Button.svelte
+++ frontend/src/components/Button.svelte
@@ -7,7 +7,7 @@
 </style>
 
 {#if type == 'primary'}
-<button class="bg-brown text-sm font-bold text-white py-4 px-12 rounded-full">{label}</button>
+<button class="bg-peach text-sm font-bold text-white py-4 px-12 rounded-full">{label}</button>
 {:else}
-<button class="bg-brown text-sm font-bold text-white py-4 px-12 rounded-full">{label}</button>
+<button class="bg-peach text-sm font-bold text-white py-4 px-12 rounded-full">{label}</button>
 {/if}
